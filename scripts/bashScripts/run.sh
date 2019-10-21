755d352d64afa9933b69791c9fa72d2b389369a0
gleisonsdm <gleison14051994@gmail.com>
2019-10-21 15:56:01 -0300

    Adding scripts to run the framework.

---
+++ scripts/bashScripts/run.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Creates a sequential version
+bash create_sequential_versions.sh
+
+# Generates reports to Autopar
+bash create_autopar_reports.sh
+
+# Generate reports to DawnCC
+bash create_dawncc_reports.sh
+
+# Generate reports to ICC
+bash create_icc_reports.sh
+
+# Generate JSONs to ICC
+bash create_icc_jsons.sh
+
+# Generate reports to Cetus
+bash create_cetus_reports.sh
+
+# Generate JSONs to represent source codes 
+bash create_source_jsons.sh
+
+# Generate the ground truth
+#  - Manual version
+#  - Then, uses ICC without threshold to find all
+#    parallelizable but not profitable loops
+bash create_ground_truth.sh
+
