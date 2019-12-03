3855c078c4c7ca8038b1032808332845a5035161
gleisonsdm <gleison14051994@gmail.com>
2019-12-03 13:12:39 -0300

    Fixing bugs on detailed reports.

--- scripts/bashScripts/create_detailed_report.sh
+++ scripts/bashScripts/create_detailed_report.sh
@@ -9,76 +9,230 @@ cd "${THIS}"
 }
 
 create_detailed_header() {
-TOOL=${1}
-if [ -f "${THIS}/reports/Detailed-Report-${TOOL}.md" ]; then
-  rm "${THIS}/reports/Detailed-Report-${TOOL}.md"
+BENCHMARK=${1}
+if [ -f "${THIS}/reports/Detailed-Report-${BENCHMARK}.md" ]; then
+  rm "${THIS}/reports/Detailed-Report-${BENCHMARK}.md"
 fi
 
-echo "# Detailed report" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "## Evaluation platform" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "Intel(R) Xeon(R) CPU E5-2686 v4" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
+echo "# Detailed report" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+echo "## Evaluation platform" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+echo "Intel(R) Xeon(R) CPU E5-2686 v4" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+}
+
+add_common_info() {
+  input=${1}
+  reference=${2}
+  while read line; do
+    # info="${line//*|}"
+    #file="${line%|*}"
+
+    ## Parser all info of a single line
+    correct="${line##*| }"
+    line_r="${line%|*}"
+    info="${line_r##*| }"
+    line_r="${line_r%|*}" 
+    loop_nl="${line_r##*| }" 
+    index="${line_r}"
+    line_r="${line_r%|*}"
+    loop_id="${line_r##*| }"
+    line_r="${line_r%|*}"
+    file="${line_r##*| }"
+
+    ## Parser the index, to be able to use it on bash scripts
+    index="${file//\./_}"
+    index="${index}_${loop_id}_${loop_nl}"
+ 
+    ## Parser the file name, to make possible to find it inside the repo
+    filename="${file}"
+    filename=${filename/.json/}
+    filename=${filename/.\/CPU\//}
+    filename=${filename/.\/GPU\//}
+    filename=${filename/.\/CPU_VECTORIZATION\//}
+    filename=${filename// /} 
+
+    filename_id="${file}"
+    filename_id=${filename_id/.json/}
+    filename_id=${filename_id/.\//}
+    filename_id=${filename_id// /}
+
+
+    link_orig="[Original](../../benchmarks/original/${filename})"
+    link_seq="[Sequential](../../benchmarks/sequential/${filename})"
+    link_ref="[Reference JSON](../../benchmarks/${reference}/${filename/.c/.c.json})" 
+    link_ground_truth="(../../benchmarks/${reference}/${filename})"
+    ## Add common info for all benchamrks
+    common_info="${filename_id} | ${loop_id}| ${loop_nl}| [${correct}]${link_ground_truth/ /} | ${link_ref} | ${link_orig} | ${link_seq}"
+    map[$index]="${common_info}"
+  done < "${input}"
+}
+
+read_file() {
+  input=${1}
+  TOOL_DIR=${2}
+
+  declare -A map_used=()
+  for index in "${!map[@]}"; do
+    map_used[$index]="false"
+  done
+
+  while read line; do
+    # info="${line//*|}"
+    #file="${line%|*}"
+
+    ## Parser all info of a single line
+    correct="${line##*| }"
+    line_r="${line%|*}"
+    info="${line_r##*| }"
+    line_r="${line_r%|*}" 
+    loop_nl="${line_r##*| }" 
+    index="${line_r}"
+    line_r="${line_r%|*}"
+    loop_id="${line_r##*| }"
+    line_r="${line_r%|*}"
+    file="${line_r##*| }"
+
+    ## Parser the index, to be able to use it on bash scripts
+    #index="${index/\./}"
+    #index="${index//\./_}"
+    #index="${index// /_}"
+    #index="${index//|/_}"
+    #index="${index//\//_}"
+    index="${file//\./_}"
+    index="${index}_${loop_id}_${loop_nl}"
+
+    filename=${file}
+    filename=${filename/.json/}
+    filename=${filename/.\/CPU\//}
+    filename=${filename/.\/GPU\//}
+    filename=${filename/.\/CPU_VECTORIZATION\//}
+    filename=${filename// /}
+
+    link_tool="[out](../../benchmarks/${TOOL_DIR}/${filename})"
+    if echo "$input" | grep -q "ICC"; then
+      link_tool="[out](../../benchmarks/${TOOL_DIR}/${filename}.optrpt)"
+    fi
+    link_tool_json="[json](../../benchmarks/${TOOL_DIR}/${filename/.c/.c.json})"
+
+    ## Add common info for all benchamrks
+
+    row="${map[$index]} | ${info} ${link_tool} ${link_tool_json}"
+    map[$index]="${row}"
+    map_used[$index]="true"
+  done < "${input}"
+
+  for index in "${!map[@]}"; do
+    if [ "${map_used[$index]}" == "false" ]; then
+      row="${map[$index]} | Not Available - -"
+      map[$index]="${row}"
+    fi
+  done
+
+  unset map_used
+}
+
+print_map() {
+  BENCHMARK="${1}"
+
+  for index in "${!map[@]}"; do
+    echo "${index}" &>> "${SCRIPTS}/list_index.txt"
+  done
+  
+  sort "${SCRIPTS}/list_index.txt" > "${SCRIPTS}/list_ordered.txt"
+  
+  COUNTER=1;
+  while IFS= read -r index
+  do
+    echo "${COUNTER} | ${map[$index]}" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+    COUNTER=$((COUNTER+1))
+  done < "${SCRIPTS}/list_ordered.txt"
+
+  rm "${SCRIPTS}/list_index.txt" "${SCRIPTS}/list_ordered.txt"
 }
 
 create_detailed_report() {
-TOOL=${1}
-REFERENCE=${2}
+  declare -A map=()
+  add_common_info "${THIS}/reports/detailed/detailed_report_Ground_Truth_DataRaceBench.txt" "reference_cpu_threading" 
+  read_file "${THIS}/reports/detailed/detailed_report_Autopar_DataRaceBench.txt" "Autopar"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Full_DataRaceBench.txt" "ICC_Full"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Cost_DataRaceBench.txt" "ICC_Cost"
+  read_file "${THIS}/reports/detailed/detailed_report_Cetus_DataRaceBench.txt" "Cetus"
 
-create_detailed_header "${TOOL}"
-echo "### ${TOOL/_/ } Report" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo " ID | Filename | Original | Sequential | Reference | Loop ID | Line Number | ${TOOL/_/ } | Output | JSON | Ground Truth " &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- " &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
+  BENCHMARK="Dataracebench"
+  create_detailed_header "${BENCHMARK}"
+  echo "### DataRaceBench Report" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | Autopar | ICC Full | ICC Cost | Cetus " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
 
-COUNTER=1
-while IFS= read -r index
-do
-  filename=${index%%|*}
-  filename=${filename/.json/}
-  filename=${filename/.\/CPU\//}
-  filename=${filename/.\/GPU\//}
-  filename=${filename/.\/CPU_VECTORIZATION\//}
-  filename=${filename// /}
+  print_map "${BENCHMARK}" 
+  unset map
 
-  info="${index#*|}"
+  declare -A map=()
+  add_common_info "${THIS}/reports/detailed/detailed_report_Ground_Truth_NPB.txt" "reference_cpu_threading" 
+  read_file "${THIS}/reports/detailed/detailed_report_Autopar_NPB.txt" "Autopar"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Full_NPB.txt" "ICC_Full"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Cost_NPB.txt" "ICC_Cost"
+  read_file "${THIS}/reports/detailed/detailed_report_Cetus_NPB.txt" "Cetus"
 
-  link_orig="[Original](../../benchmarks/original/${filename})"
-  link_seq="[Sequential](../../benchmarks/sequential/${filename})"
-  link_ref="[Reference](../../benchmarks/${REFERENCE}/${filename/.c/.c.json})"
+  BENCHMARK="NPB"
+  create_detailed_header "${BENCHMARK}"
+  echo "### NPB Report" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | Autopar | ICC Full | ICC Cost | Cetus " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
 
-  link_tool="[out](../../benchmarks/${TOOL}/${filename})"
-  if echo "$TOOL" | grep -q "ICC"; then
-    link_tool="[out](../../benchmarks/${TOOL}/${filename}.optrpt)"
-  fi
-  link_tool_json="[json](../../benchmarks/${TOOL}/${filename/.c/.c.json})"
+  print_map "${BENCHMARK}" 
+  unset map
 
-  link_ground_truth="(../../benchmarks/original/${filename})"
+  declare -A map=()
+  add_common_info "${THIS}/reports/detailed/detailed_report_Ground_Truth_Rodinia.txt" "reference_cpu_threading"
+  read_file "${THIS}/reports/detailed/detailed_report_Autopar_Rodinia.txt" "Autopar"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Full_Rodinia.txt" "ICC_Full"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Cost_Rodinia.txt" "ICC_Cost"
+  read_file "${THIS}/reports/detailed/detailed_report_Cetus_Rodinia.txt" "Cetus"
 
-  row_data="${index#*|}"
-  row_data="${row_data/ | YES/}"
-  row_data="${row_data/ | NO/}"
-  row_ground_truth="${index##*|}"
-  row_ground_truth="${row_ground_truth/ /}"
-  
-  echo "${COUNTER} | ${filename/.\//} | ${link_orig} | ${link_seq} | ${link_ref} |${row_data} | ${link_tool} | ${link_tool_json} | [${row_ground_truth}]${link_ground_truth}" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
+  BENCHMARK="Rodinia"
+  create_detailed_header "${BENCHMARK}"
+  echo "### Rodinia Report" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | DawnCC " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
 
-  COUNTER=$((COUNTER+1))
-done < "${THIS}/reports/detailed/detailed_report_${TOOL}.txt"
+  print_map "${BENCHMARK}"
+  unset map
 
-echo "" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
-echo "" &>> ${THIS}/reports/Detailed-Report-${TOOL}.md
+  declare -A map=()
+  add_common_info "${THIS}/reports/detailed/detailed_report_Dawncc.txt" "reference_gpu_target"
+  read_file "${THIS}/reports/detailed/detailed_report_Dawncc.txt" "Dawncc"
+
+  BENCHMARK="GPU_Target"
+  create_detailed_header "${BENCHMARK}"
+  echo "### DataRaceBench Report" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | ICC Simd " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+
+  print_map "${BENCHMARK}"
+  unset map
+
+  declare -A map=()
+  add_common_info "${THIS}/reports/detailed/detailed_report_ICC_Simd.txt" "reference_cpu_simd"
+  read_file "${THIS}/reports/detailed/detailed_report_ICC_Simd.txt" "ICC_Simd"
+  BENCHMARK="CPU_Vectorization"
+  create_detailed_header "${BENCHMARK}"
+  echo "### DataRaceBench Report" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo "" &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | Autopar | ICC Full | ICC Cost | Cetus " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+  echo " --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- " &>> ${THIS}/reports/Detailed-Report-${BENCHMARK}.md
+
+  print_map "${BENCHMARK}"
+  unset map
 }
-
 
 set_environment
 
-create_detailed_report "Autopar" "reference_cpu_threading"
-create_detailed_report "Cetus" "reference_cpu_threading"
-create_detailed_report "ICC_Full" "reference_cpu_threading"
-create_detailed_report "ICC_Cost" "reference_cpu_threading"
+create_detailed_report
 
-create_detailed_report "ICC_Simd" "reference_cpu_simd"
-
-create_detailed_report "Dawncc" "reference_gpu_target"
--- scripts/bashScripts/reports/Detailed-Report-GPU_Target.md
+++ scripts/bashScripts/reports/Detailed-Report-GPU_Target.md
@@ -6,7 +6,7 @@ Intel(R) Xeon(R) CPU E5-2686 v4
 
 ### DataRaceBench Report
 
- Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | Autopar | ICC Full | ICC Cost | Cetus 
+ Filename | Loop ID | Line Number | Ground Truth | JSON Reference | Original | Sequential | ICC Simd 
  --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- 
 1 | GPU/dataracebench/DRB001-antidep1-orig-yes.c | 1 | 62 | [YES](../../benchmarks/reference_gpu_target/dataracebench/DRB001-antidep1-orig-yes.c) | [Reference JSON](../../benchmarks/reference_gpu_target/dataracebench/DRB001-antidep1-orig-yes.c.json) | [Original](../../benchmarks/original/dataracebench/DRB001-antidep1-orig-yes.c) | [Sequential](../../benchmarks/sequential/dataracebench/DRB001-antidep1-orig-yes.c) | TP  [out](../../benchmarks/Dawncc/dataracebench/DRB001-antidep1-orig-yes.c) [json](../../benchmarks/Dawncc/dataracebench/DRB001-antidep1-orig-yes.c.json)
 2 | GPU/dataracebench/DRB001-antidep1-orig-yes.c | 2 | 65 | [NO](../../benchmarks/reference_gpu_target/dataracebench/DRB001-antidep1-orig-yes.c) | [Reference JSON](../../benchmarks/reference_gpu_target/dataracebench/DRB001-antidep1-orig-yes.c.json) | [Original](../../benchmarks/original/dataracebench/DRB001-antidep1-orig-yes.c) | [Sequential](../../benchmarks/sequential/dataracebench/DRB001-antidep1-orig-yes.c) | TN  [out](../../benchmarks/Dawncc/dataracebench/DRB001-antidep1-orig-yes.c) [json](../../benchmarks/Dawncc/dataracebench/DRB001-antidep1-orig-yes.c.json)
